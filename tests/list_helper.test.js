f9d699362e50ab9514970a020d7590d420472c89
Lauri Viitanen <lauri.viitanen@tuni.fi>
2022-02-24 16:38:16 +0200

    4.3 - 3.7 done: Added helper-functions and test cases for them

---
+++ tests/list_helper.test.js
@@ -0,0 +1,48 @@
+const listHelper = require('../utils/list_helper')
+
+const listOfBlogs = [
+   { likes: 1, author: "A" },
+   { likes: 2, author: "A" },
+   { likes: 11, author: "B" },
+   { likes: 0, author: "C"},
+   { likes: null, author: "C"},
+   { author: "C"},
+   {}
+]
+
+describe('sum of likes', () => {
+   test('empty array', () => {
+     expect(listHelper.totalLikes([])).toBe(0)
+   })
+   test('array of blogs array', () => {
+     expect(listHelper.totalLikes(listOfBlogs)).toBe(14)
+   })
+})
+
+describe('most liked blog', () => {
+   test('empty array', () => {
+     expect(listHelper.favoriteBlog([])).toBe(null)
+   })
+   test('array of blogs array', () => {
+     expect(listHelper.favoriteBlog(listOfBlogs)).toEqual({ author: "B", likes: 11 })
+   })
+})
+
+describe('most liked author', () => {
+   test('empty array', () => {
+     expect(listHelper.mostLikes([])).toBe(null)
+   })
+   test('array of blogs array', () => {
+     expect(listHelper.mostLikes(listOfBlogs)).toEqual({ author: "B", likes: 11 })
+   })
+})
+
+describe('with most blogs', () => {
+   test('empty array', () => {
+     expect(listHelper.mostBlogs([])).toBe(null)
+   })
+   test('array of blogs array', () => {
+     expect(listHelper.mostBlogs(listOfBlogs)).toEqual({ author: "C", blogs: 3 })
+   })
+})
+
---
+++ utils/list_helper.js
@@ -0,0 +1,44 @@
+
+const totalLikes = blogs => {
+   return blogs.map(blog => blog?.likes || 0).reduce((prev, current) => prev + current, 0);
+}
+
+const favoriteBlog = blogs => {
+   return blogs?.reduce((prev, current) => prev?.likes > (current?.likes || 0) ? prev : current, null)
+}
+
+const mostLikes = blogs => {
+   if (!blogs || blogs.length === 0) return null
+
+   const authors = [...new Set(blogs.map(b => b.author))]
+   let authorWithMostLikes = { author: "", likes: 0 }
+   authors.forEach(author => {
+      const authorsBlogs = blogs.filter(blog => blog.author === author)
+      const authorsLikes = authorsBlogs.map(blog => blog.likes).reduce((prev, current) => prev + current, 0)
+      if (authorsLikes > authorWithMostLikes.likes) {
+         authorWithMostLikes = { author: author, likes: authorsLikes}
+      }
+   })
+   return authorWithMostLikes
+}
+
+const mostBlogs = blogs => {
+   if (!blogs || blogs.length === 0) return null
+
+   const authors = [...new Set(blogs.map(b => b.author))]
+   let authorWithMostBlogs = { author: "", blogs: 0 }
+   authors.forEach(author => {
+      const authorsBlogs = blogs.filter(blog => blog.author === author)
+      if (authorsBlogs.length > authorWithMostBlogs.blogs) {
+         authorWithMostBlogs = { author: author, blogs: authorsBlogs.length}
+      }
+   })
+   return authorWithMostBlogs
+}
+ 
+module.exports = {
+   totalLikes,
+   favoriteBlog,
+   mostLikes,
+   mostBlogs
+}
